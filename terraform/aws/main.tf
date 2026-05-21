9c5f4fb95519b8fa0f58ec2c0212b7d38f27d0c8
Vyom <vyomchauhan@icloud.com>
2026-05-21 16:01:08 +1000

    Import S3 bucket encryption setting into Terraform

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -36,6 +36,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "property_landing"
   bucket = aws_s3_bucket.property_landing.id
 
   rule {
+    bucket_key_enabled = true
+
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
